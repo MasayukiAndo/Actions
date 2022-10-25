6148a6c30351b75acd47d8e7d276491a05a200e4
Masayuki Ando <48980280+MasayukiAndo@users.noreply.github.com>
2022-10-25 19:29:09 +0900

    test

--- showEnv.js
+++ showEnv.js
@@ -4,3 +4,7 @@ console.log(env.TAGS);
 
 console.log('ENV_NAME_IS');
 console.log(env.ENV_NAME_IS);
+console.log('FIREBASE_SECRETS');
+console.log(env.FIREBASE_SECRETS);
+console.log('SECRETS_TEST_STRING');
+console.log(env.SECRETS_TEST_STRING);
